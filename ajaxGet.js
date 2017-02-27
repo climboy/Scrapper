6486d45638c263f9063929cedb4a548c941b89ab
root <root@vps355652.ovh.net>
2017-02-27 20:22:10 +0100

    ajout de fichier

--- ajaxGet.js
+++ ajaxGet.js
@@ -1,79 +1,86 @@
 // Utilisation de la librairie spin.js pour générer le spinner
-
 var opts = {
-  lines: 13 // The number of lines to draw
-, length: 28 // The length of each line
-, width: 14 // The line thickness
-, radius: 42 // The radius of the inner circle
-, scale: 1 // Scales overall size of the spinner
-, corners: 1 // Corner roundness (0..1)
-, color: '#000' // #rgb or #rrggbb or array of colors
-, opacity: 0.25 // Opacity of the lines
-, rotate: 0 // The rotation offset
-, direction: 1 // 1: clockwise, -1: counterclockwise
-, speed: 1 // Rounds per second
-, trail: 60 // Afterglow percentage
-, fps: 20 // Frames per second when using setTimeout() as a fallback for CSS
-, zIndex: 2e9 // The z-index (defaults to 2000000000)
-, className: 'spinner' // The CSS class to assign to the spinner
-, top: '50%' // Top position relative to parent
-, left: '50%' // Left position relative to parent
-, shadow: false // Whether to render a shadow
-, hwaccel: false // Whether to use hardware acceleration
-, position: 'absolute' // Element positioning
+    lines: 13 // The number of lines to draw
+    , length: 28 // The length of each line
+    , width: 14 // The line thickness
+    , radius: 42 // The radius of the inner circle
+    , scale: 1 // Scales overall size of the spinner
+    , corners: 1 // Corner roundness (0..1)
+    , color: '#000' // #rgb or #rrggbb or array of colors
+    , opacity: 0.25 // Opacity of the lines
+    , rotate: 0 // The rotation offset
+    , direction: 1 // 1: clockwise, -1: counterclockwise
+    , speed: 1 // Rounds per second
+    , trail: 60 // Afterglow percentage
+    , fps: 20 // Frames per second when using setTimeout() as a fallback for CSS
+    , zIndex: 2e9 // The z-index (defaults to 2000000000)
+    , className: 'spinner' // The CSS class to assign to the spinner
+    , top: '50%' // Top position relative to parent
+    , left: '50%' // Left position relative to parent
+    , shadow: false // Whether to render a shadow
+    , hwaccel: false // Whether to use hardware acceleration
+    , position: 'absolute' // Element positioning
 }
 
 
 
 $("#form").submit(function(e){
-        e.preventDefault();
-        console.log("test");
+    e.preventDefault();
 
-     var url = $("#url").val();
-     var base_url = url;
-     var balise = $("#balise").val();
-     url = encodeURIComponent(url);
-     console.log("test ==> ", url);
-
+    var url = $("#url").val();
+    var base_url = url;
+    var balise = $("#balise").val();
+    url = encodeURIComponent(url);
+    balise = encodeURIComponent(balise);
+    var myTag = $("#tag").prop("checked");
+    var myId = $("#id").prop("checked");
+    var myClass = $("#class").prop("checked");
+    
     var target = document.getElementById('chargement');
     var spinner = new Spinner(opts).spin(target);
     $("#affichage").fadeOut(1000);
     $("#affichage").empty();
+    $("#telechargement").fadeOut(1000);
+    $("#telechargement").empty();
     $("#chargement").fadeIn(1000);
-     var string;
-     $.getJSON( 'http://localhost:8080/'+url+'/'+balise, function(result){
+    var string;
+    var type = (myTag == true) ? "" : (myId == true) ? "/id" : "/class";
+    console.log('http://vps355652.ovh.net:8080/' + url + type + '/' + balise);
+    $.getJSON('http://vps355652.ovh.net:8080/' + url + type +  '/' + balise, function(result){
+	console.log(result);
         $.each(result, function(i, field){
-          string = "<div class='content'>";
-
+            string = "<div class='content'>";
+	    
             $.each(field, function(i, value){
-              if (i == "href") {
-                if (value[0] == '/' || value[0] == '#') {
-                string = string + "<p class='line'><p class='clef'>" + i + ":</p><p class='valeur'><a href='" + base_url + value + "'>" + value + "</a></p></p>";
-              }
-              else {
-                string = string + "<p class='line'><p class='clef'>" + i + ":</p><p class='valeur'><a href='" + value + "'>" + value + "</a></p></p>";
-              }
-              }
-              else if (i == "html") {
-                string = string + "<p class='line'><p class='clef'>" + i + ":</p><p class='valeur'><code>" + value + "</code></p></p>";
+		if (i == "href") {
+                    if (value[0] == '/' || value[0] == '#') {
+			string = string + "<p class='line'><p class='clef'>" + i + ":</p><p class='valeur'><a href='" + base_url + value + "'>" + value + "</a></p></p>";
+		    } else {
+			string = string + "<p class='line'><p class='clef'>" + i + ":</p><p class='valeur'><a href='" + value + "'>" + value + "</a></p></p>";
+		    }
+		}
+		else if (i == "html") {
+                    string = string + "<p class='line'><p class='clef'>" + i + ":</p><p class='valeur'>" + value + "</p></p>";
+		    
+		} else {
+                    string = string + "<p class='line'><p class='clef'>" + i + ":</p><p class='valeur'>" + value + "</p></p>";
 
-              } else {
-                string = string + "<p class='line'><p class='clef'>" + i + ":</p><p class='valeur'>" + value + "</p></p>";
-
-              }
+		}
             })
 
-            string = string + "</div>"
+		console.log(string);
+		string = string + "</div>";
             $('#affichage').append(string+"<br />");
-            console.log("test ==> ", string);
+	    
         });
 
-     $("#chargement").fadeOut(1500);
-     $("#affichage").fadeIn(1500);
-     console.log("test");
-
+	$('#telechargement').append("<a href='http://vps355652.ovh.net:8080/download/" + url + type + "/" + balise + "' download='" + balise + ".csv'>Telecharger au format csv</a>");
+	$("#chargement").fadeOut(1500);
+	$("#telechargement").fadeIn(1500);
+	$("#affichage").fadeIn(1500);
+	
     });
 
     console.log("test");
-     return false;
-   });
+    return false;
+});
--- server.js
+++ server.js
@@ -1,72 +1,267 @@
-
 var cheerio = require('cheerio')// equivalent à JQuery ce serveur
-
 var request = require('request')// Permettre d'envoyer des requêtes à d'autres pages web ou sites web
-
 var express = require('express')// Framwork pour créer un serveur
-
 
 
 var app = express() // On crée un serveur
 
+
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
+
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+    
 });
+
 
 app.use('/', express.static(__dirname))
 
-// Géstion des routes  par express
 
+// Géstion des routes  par express
 app.get('/', function (req, res) {
-	res.sendFile('index.html')
+
+    res.sendFile('index.html')
+
 })
+
 
 app.get('/:url'/*route*/, function (req, res) {
 
-	var error = {"error": true, "message": "Merci de choisir une balise"}
-	res.json(error)
+    var error = {"error": true, "message": "Merci de choisir une balise"}
+    res.json(error)
+    
 })
 
+
+app.get('/download/:url/id/:balise', function (req, res) {
+    var url = req.params.url
+    var balise = "#" + req.params.balise
+    url = decodeURIComponent(url)
+    url = getEntireURI(url)
+    console.log(url);
+    console.log("balise ==>>", balise)
+    request(url, function (error, response, body) {
+  	if (!error && response.statusCode == 200) {
+    	    $ = cheerio.load(body)// Show the HTML for the Google homepage.
+	    console.log($(balise))
+    	    var tab = "attribute,text,html\n"
+	    $(balise).each(function(i, elem) {
+		var content = ""
+    		for (key in elem.attribs) {
+		    var str = (elem.attribs[key]).replace(/\s{2,}/g, " ");
+    		    content = content + key + "='" + str + "'/"
+    		}
+    		content = content + "," + $(this).text() + ","
+    		content = content + $(this).html() + ","
+		tab = tab + content + "\n"
+	    })
+		console.log(tab)
+		res.send(tab)
+	}
+	else {
+	    console.log("Error received")
+	    console.log(error)
+	}
+    })
+    console.log("end")
+})
+
+
+app.get('/download/:url/class/:balise', function (req, res) {
+    var url = req.params.url
+    var balise = "." + req.params.balise
+    url = decodeURIComponent(url)
+    url = getEntireURI(url)
+    console.log(url);
+    console.log("balise ==>>", balise)
+    request(url, function (error, response, body) {
+  	if (!error && response.statusCode == 200) {
+    	    $ = cheerio.load(body)// Show the HTML for the Google homepage.
+	    console.log($(balise))
+    	    var tab = "attribute,text,html\n"
+	    $(balise).each(function(i, elem) {
+		var content = ""
+    		for (key in elem.attribs) {
+    		    content = content + key + "='" + elem.attribs[key] + "'/"
+    		}
+    		content = content + "," + $(this).text() + ","
+    		content = content + $(this).html() + ","
+		tab = tab + content + "\n"
+	    })
+		console.log(tab)
+		res.send(tab)
+	}
+	else {
+	    console.log("Error received")
+	    console.log(error)
+	}
+    })
+    console.log("end")
+})
+
+
+app.get('/download/:url/:balise', function (req, res) {
+    var url = req.params.url
+    var balise = req.params.balise
+    url = decodeURIComponent(url)
+    url = getEntireURI(url)
+    console.log(url);
+    console.log("balise ==>>", balise)
+    request(url, function (error, response, body) {
+  	if (!error && response.statusCode == 200) {
+    	    $ = cheerio.load(body)// Show the HTML for the Google homepage.
+	    console.log($(balise))
+    	    var tab = "attribute,text,html\n"
+	    $(balise).each(function(i, elem) {
+		var content = ""
+    		for (key in elem.attribs) {
+    		    content = content + key + "='" + elem.attribs[key] + "'/"
+    		}
+    		content = content + "," + $(this).text() + ","
+    		content = content + $(this).html() + ","
+		tab = tab + content + "\n"
+	    })
+		console.log(tab)
+		res.send(tab)
+	}
+	else {
+	    console.log("Error received")
+	    console.log(error)
+	}
+    })
+    console.log("end")
+})
 
 
 app.get('/:url/:balise', function (req, res) {
+    var url = req.params.url
+    var balise = req.params.balise
+    url = decodeURIComponent(url)
+    url = getEntireURI(url)
+    console.log(url);
+    console.log("balise ==>>", balise)
+    request(url, function (error, response, body) {
+  	if (!error && response.statusCode == 200) {
+    	    $ = cheerio.load(body)// Show the HTML for the Google homepage.
+	    console.log($(balise))
+    	    var tab = []
+	    $(balise).each(function(i, elem) {
+		var content = {}
+    		for (key in elem.attribs) {
+    		    content[key] = elem.attribs[key]
+    		}
+    		content.text = $(this).text()
+    		content.html = $(this).html()
+		tab[i] = content
+	    })
+		console.log(tab);
+		res.json(tab)
+	}
+	else {
+	    console.log("Error received")
+	    console.log(error)
+	}
+    })
+    console.log("end")
+})
 
-	var url = req.params.url
-	var balise = req.params.balise
+app.get('/:url/class/:balise', function (req, res) {
+    var url = req.params.url
+    var balise = "." + req.params.balise
+    url = decodeURIComponent(url)
+    url = getEntireURI(url)
+    console.log(url)
+    console.log(balise)
+    request(url, function (error, response, body) {
+  	if (!error && response.statusCode == 200) {
+    	    $ = cheerio.load(body)// Show the HTML for the Google homepage.
+    	    var tab = []
+	    console.log("balise ==>>", balise)
+	    console.log($(balise))
+	    $(balise).each(function(i, elem) {
+		var content = {}
+    		for (key in elem.attribs) {
+    		    content[key] = elem.attribs[key]
+    		}
+		console.log($(this).html())
+    		content.text = htmlEntities($(this).text())
+    		content.html = htmlEntities($(this).html())
+		tab[i] = content
+	    })
+		console.log("tab ==>>")
+		console.log(tab)
+		res.json(tab)
+	}
+	else {
+	    console.log("Error received")
+	    console.log(error)
+	}
+    })
+    console.log("end")
+})
 
-	url =  decodeURIComponent(url);
 
-	request(url, function (error, response, body) {
-  		if (!error && response.statusCode == 200) {
-    		$ = cheerio.load(body)// Show the HTML for the Google homepage.
-
-    		// Ce tableau nous permet de stocker toutes les infos des balises 
-    		var tab = []
-
-    		// La variable balise est initialisée plus haut
-    		// cheerio va s'occuper de récuperer les infos sur la balise
-			$(balise).each(function(i, elem) {
-				// On récupère  les infos dans un tableau objet
-				var content = {}
-    			for (key in elem.attribs) {
-    				content[key] = elem.attribs[key];
-    			}
-    		
-    			content.text = $(this).text()
-    			content.html = $(this).html()
-			  	tab[i] = content;
-			});
-		
-			
-			// Renvoie la reponse à l'utilisateur
-			res.json(tab)
-    	}
-	})
-
+app.get('/:url/id/:balise', function (req, res) {
+    var url = req.params.url
+    var balise = "#" + req.params.balise
+    url = decodeURIComponent(url)
+    url = getEntireURI(url)
+    console.log(url)
+    console.log(balise)
+    request(url, function (error, response, body) {
+  	if (!error && response.statusCode == 200) {
+    	    $ = cheerio.load(body)// Show the HTML for the Google homepage.
+    	    var tab = []
+	    console.log("balise ==>>", balise)
+	    console.log($(balise))
+	    $(balise).each(function(i, elem) {
+		var content = {}
+    		for (key in elem.attribs) {
+    		    content[key] = elem.attribs[key]
+    		}
+		console.log($(this).html())
+    		content.text = htmlEntities($(this).text())
+    		content.html = htmlEntities($(this).html())
+		tab[i] = content
+	    })
+		console.log("tab ==>>")
+		console.log(tab)
+		res.json(tab)
+	}
+	else {
+	    console.log("Error received")
+	    console.log(error)
+	}
+    })
+    console.log("end")
 })
 
 app.listen(8080, function() {
-	console.log('ça tourne')
+
+    console.log('ça tourne')
+
 })
+
+function getEntireURI(uri) {
+    var url = "";
+
+    if (uri.substring(0, 12) == "https://www." ||
+	uri.substring(0, 11) == "http://www." || 
+	uri.substring(0, 8) == "https://") {
+
+	return uri;
+
+    } else {
+
+	if (uri.substring(0, 4) == "www.")
+	    return "http://" + uri;
+
+	return "http://www." + uri;
+
+    }
+}
+
+function htmlEntities(str) {
+    return String(str).replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;');
+}
